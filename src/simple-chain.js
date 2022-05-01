bdd93567e7344e3855e4d4442f81389c8b0caac7
Vlad Horban <ywvvvw@gmail.com>
2022-05-01 16:23:06 +0300

    feat: add simple-chain, vigenere-cipher solutions

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,28 +2,34 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  result: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.result.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.result.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (Number.isInteger(position) && position > 0 && position < this.result.length) {
+        this.result.splice(position - 1, 1);
+    } else {
+        this.result = [];
+        throw new Error ("You can't remove incorrect link!");
+    }
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.result.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let res = this.result.join("~~");
+    this.result = [];
+    return res;
   }
 };
 
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,30 +3,69 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(string, key) {
+    if ((string === undefined) || (key === undefined)) {
+      throw new SyntaxError('Incorrect arguments!');
+    }
+    string = string.toLowerCase();
+    key = key.padEnd(string.length, key).toLowerCase();
+    let res = '';
+    let k = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+        res += String.fromCharCode(97 + (string.charCodeAt(i) + key.charCodeAt(k) - 2 * 97) % 26);
+        k++;
+      }
+      else {
+        res += string[i];
+      }
+    }
+    if (!this.type) {
+      res = res.split('').reverse().join('');
+    }
+    return res.toUpperCase();
+  }
+  decrypt(string, key) {
+    if ((string === undefined) || (key === undefined)) {
+      throw new SyntaxError('Incorrect arguments!');
+    }
+    string = string.toLowerCase();
+    key = key.padEnd(string.length, key).toLowerCase();
+    let res = '';
+    let k = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+        res += String.fromCharCode(97 + (26 + string.charCodeAt(i) - key.charCodeAt(k)) % 26);
+        k++;
+      }
+      else {
+        res += string[i];
+      }
+    }
+    if (!this.type) {
+      res = res.split('').reverse().join('');
+    }
+    return res.toUpperCase();
   }
 }
 
